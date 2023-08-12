8290f898dbdf20e0688e5e6f652cbfda7c2d30b2
Dreyrk <lucas.rondepierre123@gmail.com>
2023-08-12 10:31:25 +0200

    scrollbar styled

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -9,13 +9,13 @@ function Section({ data, title }) {
             <div className='border-b-4 border-dark-secondary'>
                 <h2 className='mb-2 ml-2 text-lg font-bold'>{title}</h2>
             </div>
-            <div className='flex gap-6 py-4 overflow-auto no-scrollbar'>
+            <div className='flex gap-6 py-4 overflow-auto max-md:no-scrollbar'>
                 {data.map((manhwa) => (<ManhwaBox manhwa={manhwa} />))}
             </div>
-            <button className='absolute grid p-1 -right-16 top-1/2 place-content-center' type="button">
+            <button className='absolute hidden p-1 lg:grid -right-16 top-1/2 place-content-center' type="button">
                 <BsChevronRight size={40} />
             </button>
-            <button className='absolute grid p-1 -left-16 top-1/2 place-content-center' type="button">
+            <button className='absolute hidden p-1 lg:grid -left-16 top-1/2 place-content-center' type="button">
                 <BsChevronLeft size={40} />
             </button>
         </section>
